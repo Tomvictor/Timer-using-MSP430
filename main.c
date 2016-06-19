467a5e7d813adcff698e1f350f318132167d823a
Tomvictor <vjtomvictor@icloud.com>
2016-06-19 18:45:11 +0530

    reg value test

--- main.c
+++ main.c
@@ -25,8 +25,8 @@ int main(void)
   TACTL = TASSEL_1 + ID_3 + MC_2;                  // ACLK, Timer A input divider: 3 - /8 ,Continous up
 
   //P1OUT |= BIT0 ;
-  CCR0 = 100;
-  CCR1 = 200;
+  CCR0 = 10;
+  CCR1 = 20;
 
   __bis_SR_register(LPM0_bits + GIE);       // Enter LPM0 w/ interrupt
 } //Main end
@@ -37,7 +37,7 @@ __interrupt void Timer_A0 (void)
 
 {
   P1OUT ^= BIT0 ;
-  CCR0 = TAR + 100 ;                              // Add Offset to CCR1
+  CCR0 = TAR + 10 ;                              // Add Offset to CCR1
 }
 
 // Timer_A2 Interrupt Vector (TA0IV) handler
@@ -51,7 +51,7 @@ __interrupt void Timer_A1(void)
   case  2:
 	  //CCR1 += 1000 ;                    // Add Offset to CCR1
 	  P1OUT ^= BIT6 ;
-	  CCR1 = TAR + 200   ;
+	  CCR1 = TAR + 20   ;
 
            break;
   case  4:
